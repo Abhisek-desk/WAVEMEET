d41ca873e1411fb381999c88b9d6a55f3c8e50f0
Abhisek Mishra <abhisekmishra720@gmail.com>
2026-02-20 22:11:01 +0530

    Ready for Deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import { createServer } from "node:http";
-
 import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
 import userRoutes from "./routes/users.routes.js";
 import { connectToSocket } from "./controllers/socketManager.js";
+const dbUrl = process.env.ATLASDB_URL;
 
 const app = express();
 const server = createServer(app); // Socket.io is tied to this
@@ -19,23 +22,21 @@ app.use(express.urlencoded({ extended: true, limit: "40kb" }));
 app.use("/api/v1/users", userRoutes);
 
 app.get("/home", (req, res) => {
-    return res.json({ hello: "world" });
+  return res.json({ hello: "world" });
 });
 
 const start = async () => {
-    try {
-        const connectionDB = await mongoose.connect(
-            "mongodb+srv://abhisekmishra720_db_user:abhisek%40%231290@cluster0.5jx6g5j.mongodb.net/",
-        );
-        console.log(`MONGO connected DB Host: ${connectionDB.connection.host}`);
+  try {
+    const connectionDB = await mongoose.connect(dbUrl);
+    console.log(`MONGO connected DB Host: ${connectionDB.connection.host}`);
 
-        // FIX: Change app.listen to server.listen
-        server.listen(app.get("port"), () => {
-            console.log(`Server is running on port ${app.get("port")}`);
-        });
-    } catch (error) {
-        console.error("Error starting the server:", error);
-    }
+    // FIX: Change app.listen to server.listen
+    server.listen(app.get("port"), () => {
+      console.log(`Server is running on port ${app.get("port")}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server:", error);
+  }
 };
 
 start();
